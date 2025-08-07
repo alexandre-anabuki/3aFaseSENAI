cde033e6d0079c7354177123e1bb7005bb632d11
alexandre-anabuki <alex.anabuki@gmail.com>
2025-08-06 22:09:47 -0300

    continuação do prisma

---
+++ clinicadb/prisma/seed.js
@@ -0,0 +1,49 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.usuario.createMany({
+        data: [
+            { nome: "Joao", email: "joao34@email.com", senha: "123", cargo: "Médico" },
+        ],
+    })
+
+    await prisma.paciente.create({
+        data: {
+            nome: "João",
+            sexo: "Masculino",
+            data_nascimento: new Date("1980-12-11"),
+            cpf:"958.521.65",
+            telefone: 952556232,
+            email: "joao@email.com",
+            // Consulta: {
+            //     create: [
+            //         {
+            //             motivo: "Dor nas costas",
+            //             data_consulta: new Date("2023-98-25"),
+            //             observacoes: "Ibuprofeno 3 vezes ao dia",
+            //         }
+            //     ]
+            // },
+            // Exame: {
+            //     create: [
+            //         {
+            //             tipo: "Densiometro",
+            //             data_exame: new Date("2013-08-25"),
+            //             resultado: "deu ruim",
+            //             link_arquivo: "url.com.br/exame.pdf",
+            //             observacoes: "",
+            //         }
+            //     ]
+            // }
+        },
+    })
+}
+
+main()
+.then(() => prisma.$disconnect())
+.catch((e) => {
+    console.error(e);
+    prisma.$disconnect();
+    process.exit(1)
+});
