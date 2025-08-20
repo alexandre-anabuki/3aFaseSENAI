7b58c3b085b0a7f0e6dfcdb90e62f705a3c6d701
alexandre-anabuki <alex.anabuki@gmail.com>
2025-08-19 22:11:08 -0300

    endpoints

--- clinicadb/src/app.js
+++ clinicadb/src/app.js
@@ -31,4 +31,62 @@ app.get('/usuarios/:id', async (request, response) =>{
     }
 })
 
+app.post("/usuarios", async(req, res)=> {
+    try {
+      const { body } = req
+      const usuario = await prismaClient.usuario.create({
+        data: {
+          nome: body.nome,
+          cargo: body.cargo,
+          email: body.email,
+          senha: body.senha
+        },
+      })
+      return res.status(201).json(usuario)
+    } catch (error) {
+      console.error(error)
+      if(error.code === "P2002"){
+        res.status(404).send("Falha ao cadastrar usuário: Email já cadastrado!")
+      }
+    }
+})
+
+app.put("/usuarios/:id", async(req, res)=>{
+    try {
+      const { body, params } = req
+
+      if(body.nome || body.cargo || body.email || body.senha){
+          await prismaClient.usuario.update({
+            where: { id: Number(params.id) },
+            data: { 
+              ...body
+             },
+          })
+          
+          const usuarioAtualizado = await prismaClient.usuario.findUnique({
+              where: {
+                  id: Number(params.id)
+                }
+            })
+            
+            res.status(201).json({
+                message: "Usuário atualizado!",
+                data: usuarioAtualizado
+            })
+      }
+      else{
+        res.status(404).send("atributos não condiz com o esquema")
+      }
+        
+    } catch (error) {
+        if(error.code === 'P2025'){
+            res.status(404).send("usuario não encontrado")
+        }
+        if(error.code === 'p2002'){
+            res.status(404).send("email ja existe")
+        }
+      console.log(error)
+    }
+})
+
 app.listen(3000, ()=> console.log("api rodando"))
