32a74d8e911d825237edb28562af77c2d893345b
alexandre-anabuki <alex.anabuki@gmail.com>
2025-09-25 22:14:28 -0300

    dependencias e tailwind

---
+++ clinica/src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import Login from './pages/Login/Login'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Login />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </StrictMode>,
+)
